e07b5b2196a400bbc1d6c5287fcb8d1713c3c85e
tk0358 <tk0358a@yahoo.co.jp>
2019-07-31 12:35:43 +0900

    Add article and category association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_005351) do
+ActiveRecord::Schema.define(version: 2019_07_31_032029) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
